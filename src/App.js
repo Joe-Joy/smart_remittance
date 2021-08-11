8bfa7d66efbb10fd2fb2bfb0ae359f9e7d335ca8
jinojoseph <jinojoseph@chevron.com>
2021-08-11 09:17:19 +0530

    comitted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Headers from "./Components/Headers/Headers";
 
 import Mainlogin from "./Components/Login/Mainlogin";
 import MainRegister from "./Components/Register/MainRegister";
+import MainSecondPart from "./Components/Headers/SecondPart/MainSecondPart";
+import SecondPartLeft from "./Components/Headers/SecondPart/SecondPartLeft";
+import SecondPartRight from "./Components/Headers/SecondPart/SecondPartRight";
 function App() {
   // const token = localStorage.getItem("user");
 
@@ -16,7 +18,7 @@ function App() {
       <Container>
         <Headers exact path="/" />
         <Switch>
-          <Route ClassName="loginn" exact path="/Login" component={Mainlogin} />
+          <Route exact path="/Login" component={Mainlogin} />
           <Route exact path="/Register" component={MainRegister} />
         </Switch>
       </Container>
--- src/Components/Headers/Headers.js
+++ src/Components/Headers/Headers.js
@@ -1,31 +1,33 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
-import { Link } from 'react-router-dom';
-
+import MainSecondPart from "./SecondPart/MainSecondPart";
+import MainThirdPart from "./HowTOGetStated/MainThirdPart";
 import "./Headers.css";
 
 const Headers = () => {
   console.log();
 
-
-  
   // const guestMenu = (
   //   <Nav.Link to="/" as={Link} >Login</Nav.Link>
   // );
 
   return (
+    <div>
       <Navbar>
         <Container className="heading">
-          <Navbar.Brand className="logo">Smart Remittance</Navbar.Brand>
+          <Navbar.Brand className="mainlogo">Smart Remittance</Navbar.Brand>
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              <a href="/moneytransfer" as={Link} >Money Transfer</a>
+              <a href="/moneytransfer">Money Transfer</a>
               <a href="/register">Register</a>
               <a href="/login">Login</a>
             </Navbar.Text>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+      <MainSecondPart />
+      <MainThirdPart />
+    </div>
   );
 };
 
